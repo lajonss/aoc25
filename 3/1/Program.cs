d9fc7ae406b437e004372aa8aeec9a3f5f6fd96b
lajonss <l3n1@dupaw.eu>
2025-12-04 00:26:49 +0100

    3.1 solution LINQ

---
+++ 3/1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(
+    Console.In.ReadToEnd()
+        .Split('\n')
+        .Where(line => !string.IsNullOrWhiteSpace(line))
+        .Select(line => line.Select(char.GetNumericValue).Select(n => (uint)n))
+        .Select(seq => seq.SkipLast(1).Index().SelectMany(first => seq.Skip(first.Index + 1).Select(second => first.Item * 10 + second)).Max())
+        .Aggregate((a, b) => a + b)
+);
