131a7f0542a67818812d2394a4491d2c79e1b945
lajonss <l3n1@dupaw.eu>
2025-12-04 02:59:06 +0100

    3.2: recursive function solution

---
+++ 3/2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(
+    Console.In.ReadToEnd()
+        .Split('\n')
+        .Where(line => !string.IsNullOrWhiteSpace(line))
+        .Select(line => line.Select(char.GetNumericValue).Select(n => (byte)n))
+        .Select(seq => Joltage(seq, 11))
+        .Aggregate((a, b) => a + b)
+);
+
+static ulong Joltage(IEnumerable<byte> seq, int numbersLeft, ulong acc = 0) {
+    if (numbersLeft < 0) {
+        return acc;
+    }
+
+    var max = seq.SkipLast(numbersLeft).Index().Max(new Comparer());
+    return Joltage(seq.Skip(max.Index + 1), numbersLeft - 1, acc * 10 + max.Item);
+}
+
+struct Comparer : IComparer<(int Index, byte Item)> {
+    public readonly int Compare((int Index, byte Item) x, (int Index, byte Item) y) {
+        if (x.Item > y.Item) {
+            return 1;
+        } else if (x.Item == y.Item) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
