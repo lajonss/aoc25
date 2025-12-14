c30bf77c94cdde8993783c81a2379adab4af50c8
lajonss <l3n1@dupaw.eu>
2025-12-14 23:53:41 +0100

    A.1

--- A-kotlin/1.kt
+++ A-kotlin/1.kt
@@ -1,13 +1,34 @@
 data class Machine(
-    val indicatorLights: List<Boolean>,
-    val buttionWirings: List<List<Int>>,
+    val indicatorLights: Long,
+    val indicatorLightsSize: Long,
+    val buttonWirings: List<Long>,
 )
 
 fun parseMachine(line: String): Machine {
     val parts = line.split(" ")
-    val indicatorLights = parts[0].drop(1).dropLast(1).map({it == '#'})
-    val buttonWirings = parts.drop(1).dropLast(1).map({it.drop(1).dropLast(1).split(",").map({Integer.parseInt(it)})})
-    return Machine(indicatorLights, buttonWirings)
+    val indicatorLightsSpec = parts[0].drop(1).dropLast(1)
+    val indicatorLights = indicatorLightsSpec.map({it == '#'}).foldRight(0L, { current, acc -> (acc shl 1) + if (current) 1 else 0})
+    val indicatorLightsSize = 1L shl indicatorLightsSpec.count()
+    val buttonWirings = parts.drop(1).dropLast(1).map({it.drop(1).dropLast(1).split(",").map({Integer.parseInt(it)}).fold(0L, {acc, current -> acc + (1 shl current)})})
+    return Machine(indicatorLights, indicatorLightsSize, buttonWirings)
+}
+
+fun getState(machine: Machine, pressedCount: Int): Sequence<Long> {
+    if (pressedCount == 0) {
+        return sequenceOf(0L)
+    }
+    return getState(machine, pressedCount -1).flatMap({outer -> machine.buttonWirings.map({(outer xor it) % machine.indicatorLightsSize})})
+}
+
+fun getPressCount(machine: Machine): Int {
+    for(pressedCount in 1..Int.MAX_VALUE) {
+        for(possibleState in getState(machine, pressedCount)) {
+            if (possibleState == machine.indicatorLights) {
+                return pressedCount
+            }
+        }
+    }
+    throw Exception()
 }
 
 fun getLine(): String? {
@@ -23,9 +44,7 @@ fun getLines(): Sequence<String> {
 }
 
 fun main() {
-    for (line in getLines().map({ parseMachine(it) })) {
-        println(line)
-    }
+    println(getLines().map(::parseMachine).map(::getPressCount).sum())
 }
 
 main()
